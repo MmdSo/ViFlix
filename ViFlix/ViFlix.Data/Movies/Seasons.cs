b2ea69f1c4a65e45d0cd50f0b66c1d2160a26c54
MmdSo <goldenscorpion3@gmail.com>
2025-04-30 04:19:01 +0330

    Series entity

---
+++ ViFlix/ViFlix.Data/Movies/Seasons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViFlix.Data.Context;
+
+namespace ViFlix.Data.Movies
+{
+    public class Seasons : BaseEntitiies
+    {
+        public int SeasonNumber { get; set; }
+        public long SeriesId { get; set; }
+        public int Episodes { get; set; }
+        public Series series { get; set; }
+
+        #region Relations
+        [ForeignKey("SeriesId")]
+        public Series serie { get; set; }
+        #endregion
+    }
+}
---
+++ ViFlix/ViFlix.Data/Movies/Series.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViFlix.Data.Context;
+
+namespace ViFlix.Data.Movies
+{
+    public class Series : BaseEntitiies
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? Poster { get; set; }  
+        public DateTime ReleaseDate { get; set; }
+        public bool IsCompleted { get; set; } 
+        public string? Link { get; set; }
+
+        #region Relations
+        public List<Reviews> reviews { get; set; }
+        public List<Ganres> ganres { get; set; }
+        public List<Seasons> season { get; set; }
+
+        #endregion
+    }
+}
