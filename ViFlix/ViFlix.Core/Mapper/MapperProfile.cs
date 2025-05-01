10b69c96293aedb00443e6faeea34f7ca95b8d52
MmdSo <goldenscorpion3@gmail.com>
2025-05-02 02:47:32 +0330

    mapper

---
+++ ViFlix/ViFlix.Core/Mapper/MapperProfile.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViFlix.Core.ViewModels.MoviesViewModel;
+using ViFlix.Core.ViewModels.SubscriptionViewModels;
+using ViFlix.Core.ViewModels.UsersViewModels;
+using ViFlix.Data.Movies;
+using ViFlix.Data.Subscription;
+using ViFlix.Data.Users;
+
+namespace ViFlix.Core.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            #region UserMapper
+            CreateMap<SiteUsers, UserViewModel>();
+            CreateMap<UserViewModel, SiteUsers>();
+
+            CreateMap<SiteUsers, RegisterViewModel>();
+            CreateMap<RegisterViewModel, SiteUsers>();
+
+            CreateMap<SiteUsers, LoginViewModel>();
+            CreateMap<LoginViewModel, SiteUsers>();
+
+            CreateMap<SiteUsers, ProfileViewModel>();
+            CreateMap<ProfileViewModel, SiteUsers>();
+
+            CreateMap<SiteUsers, ForgetPasswordViewModel>();
+            CreateMap<ForgetPasswordViewModel, SiteUsers>();
+
+            CreateMap<SiteUsers, ChangeEmailViewModel>();
+            CreateMap<ChangeEmailViewModel, SiteUsers>();
+
+            CreateMap<SiteUsers, ChangePasswordViewModel>();
+            CreateMap<ChangePasswordViewModel, SiteUsers>();
+
+            CreateMap<WishList, WishListViewModel>();
+            CreateMap<WishListViewModel, WishList>();
+
+            CreateMap<Roles, RoleViewModel>();
+            CreateMap<RoleViewModel, Roles>();
+
+            CreateMap<Permissions, PermissionViewModel>();
+            CreateMap<PermissionViewModel, Permissions>();
+            #endregion
+
+            #region Subscription
+            CreateMap<SubscriptionPlan, SuscriptionPlanViewModel>();
+            CreateMap<SuscriptionPlanViewModel, SubscriptionPlan>();
+
+            CreateMap<UserSubscription, UserSubscriptionViewModel>();
+            CreateMap<UserSubscriptionViewModel, UserSubscription>();
+            #endregion
+
+            #region movies
+            CreateMap<Ganres, GanresViewModel>();
+            CreateMap<GanresViewModel, Ganres>();
+
+            CreateMap<Language, LanguageViewModel>();
+            CreateMap<LanguageViewModel, Language>();
+
+            CreateMap<Movie, MovieViewModel>();
+            CreateMap<MovieViewModel, Movie>();
+
+            CreateMap<Reviews, ReviewViewModel>();
+            CreateMap<ReviewViewModel, Reviews>();
+
+            CreateMap<Seasons, SeasonsViewModel>();
+            CreateMap<SeasonsViewModel, Seasons>();
+
+            CreateMap<Series, SeriesViewModel>();
+            CreateMap<SeriesViewModel, Series>();
+            #endregion
+        }
+    }
+}
